188bd0dfb5739ddddb587c6e50cd50543a9e0660
RicardoCS-CPNV <ricardo.costa1@eduvaud.ch>
2024-03-08 10:18:41 +0100

    Add PlayerUI script

---
+++ Assets/Script/PlayerUI.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerUI : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform thrusterFuellFill;
+
+    private PlayerController controller;
+
+    [SerializeField]
+    private GameObject pauseMenu;
+
+
+    public void SetFuelController(PlayerController _controller)
+    {
+        controller = _controller;
+    }
+
+    void Start()
+    {
+        PauseMenu.isOn = false;
+    }
+
+    void Update()
+    {
+        //SetFuelController(controller.GetThrusterFuelAmount());
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+    }
+
+    public void TogglePauseMenu()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+        PauseMenu.isOn = pauseMenu.activeSelf;
+    }
+
+    void SetFuelAmount(float _amount)
+    {
+        thrusterFuellFill.localScale = new Vector3(1f, _amount, 1f);
+    }
+}
