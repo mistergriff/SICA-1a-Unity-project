5fa7154fe5973cea238754e2372f36a6473beafd
RicardoCS-CPNV <ricardo.costa1@eduvaud.ch>
2024-03-11 22:16:12 +0100

    Add button fonction on Pause Menu
    Resume Game
    Leave Room :
        - Host can leave and the game is over
        - Client can leave but the game continue

--- Assets/Script/PauseMenu.cs
+++ Assets/Script/PauseMenu.cs
@@ -1,8 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenu : NetworkBehaviour
 {
     public static bool isOn = false;
+
+    private NetworkManager networkManager;
+
+    private void Start()
+    {
+        networkManager = NetworkManager.singleton;
+    }
+
+    public void LeaveRoomBtn()
+    {
+        if(isClientOnly)
+        {
+            networkManager.StopClient(); //Seul le client quitte
+        }
+        else
+        {
+            networkManager.StopHost(); //L'hôte quitte donc tous les joueurs sont déconnectés
+        }
+    }
 }
