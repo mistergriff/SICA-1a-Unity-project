e9982524c70233325115b52cede7eccc05b24fa7
mistergriff <g_renaud@hotmail.ch>
2024-03-18 14:54:50 +0100

    Fix + Updated wall colliders

---
+++ Assets/Script/HostConnect.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+
+public class HostConnect : MonoBehaviour
+{
+    NetworkManager networkManager;
+    public InputField ipInput;
+    public GameObject HostConnectGo;
+
+    void Awake()
+    {
+        networkManager = GetComponent<NetworkManager>();
+    }
+
+    public void HostFunction()
+    {
+        networkManager.StartHost();
+
+        HostConnectGo.SetActive(false);
+    }
+
+    public void ConnectFunction()
+    {
+        networkManager.networkAddress = ipInput.text;
+        networkManager.StartClient();
+
+        HostConnectGo.SetActive(false);
+    }
+
+    public void ServerFunction()
+    {
+        networkManager.StartServer();
+
+        HostConnectGo.SetActive(false);
+    }
+}
